326e801b6f7d618b43b1670df079434c10cc1e8e
Esteban Villacorta <estebanvillacorta@Estebans-M4-MBP-3.local>
2026-04-03 12:55:32 +0200

    Restructure project into frontend and backend folders

---
+++ backend/planabite-backend/src/main/java/com/example/planabite_backend/planabite_backend/PlanabiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.planabite_backend.planabite_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanabiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanabiteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/planabite-backend/src/test/java/com/example/planabite_backend/planabite_backend/PlanabiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planabite_backend.planabite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanabiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
